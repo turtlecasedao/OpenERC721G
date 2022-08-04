047c9d8ceda04fe9828d8ce9bc10b3c71463c1e3
Z.Y <i-zhen@users.noreply.github.com>
2022-08-04 10:45:29 +0800

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,31 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
+require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
+
+if (process.env.REPORT_GAS) {
+  require('hardhat-gas-reporter');
+}
+
+if (process.env.REPORT_COVERAGE) {
+  require('solidity-coverage');
+}
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.15",
+  solidity: {
+    version: "0.8.15",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 15000,
+      },
+    },
+  },
+  asReporter: {
+    token: 'ETH',
+    currency: 'USD',
+    coinmarketcap: "01bc3d17-df19-4124-8478-24ba2e4bb89c",
+    enabled: process.env.REPORT_GAS ? true : false
+  },
+  plugins: ['solidity-coverage'],
 };
